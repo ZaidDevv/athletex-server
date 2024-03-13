11002a1630538274ec12e84185c047c2083c39d8
Zaid <zaidbusiness@hotmail.com>
2024-03-13 21:37:06 +0100

    Create /public directory if it doesnt exist, case when running from /dist

--- src/middleware/multer.ts
+++ src/middleware/multer.ts
@@ -1,11 +1,14 @@
 import multer from 'multer';
 import path from 'path';
-
+import fs from 'fs';
 // Set up disk storage
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        // Specify the destination folder where files will be stored
-        cb(null, 'src/public/uploads/');
+        // check if the directory exists and create it if it doesn't
+        if (!fs.existsSync(path.join(process.cwd(), "src",'public', 'uploads'))) {
+            fs.mkdirSync(path.join(process.cwd(), "src",'public', 'uploads'));
+        }
+        cb(null, path.join(process.cwd(), "src",'public', 'uploads'));
     },
     filename: (req, file, cb) => {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
